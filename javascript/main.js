1bb28984d8a93daf5865d48e1f60c95748d0c1db
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-05-05 15:44:28 -0400

    updated code

--- javascript/main.js
+++ javascript/main.js
@@ -2,13 +2,18 @@ let obj = {
   counter: 5,
 };
 
-function incrementBy(item, amount) {
+function incrementBy(item, amount) { // Pass by Reference due to object, Pass by value.
   obj.counter += amount; // Correctly changes obj.counter, as objects are passed by reference.
   amount = 0; // Fails to change what was passed as amount, as its passed by value.
-  console.log(amount); // Outputs: 0
+  console.log(amount); // Outputs: 0, but n remains unchanged.
+  // amount is technically not deallocated here, since we use garbage collection we need to wait till it runs again to deallocate.
 };
 
+// JavaScript doesn't require an entry point, it just runs what it sees.
+
 let n = 2;
-incrementBy(obj, n);
+incrementBy(obj, n); // Passes obj by reference, n by value.
 console.log(obj.counter); // Outputs: 7
 console.log(n); // Outputs: 2
+
+// All memory is garbage collected due to everything finishing.
--- rust/main.rs
+++ rust/main.rs
@@ -3,17 +3,18 @@ struct Obj {
 }
 
 fn increment_by(item: &mut Obj, amount: u32) { // Pass item by reference, amount by value.
-  (*item).counter += amount;
-  // amount = 5; <- "cannot assign to immutable argument"
-  println!("{}", amount.to_string()); // Outputs: 2
+    (*item).counter += amount; // dereference item, and increment its counter.
+ // amount = 5; <- "cannot assign to immutable argument"
+    println!("{}", amount.to_string()); // Outputs: 2
+    // amount is deallocated due to it exiting scope.
 }
 
-fn main() {
-  let mut obj = Obj {
-    counter: 5,
-  };
-  let n: u32 = 2;
-  increment_by(&mut obj, n);
-  println!("{}", obj.counter.to_string()); // Outputs: 7
-  println!("{}", n.to_string()); // Outputs: 2
+fn main() { // Entry point due to rust being compiled.
+    let mut obj = Obj {
+        counter: 5,
+    };
+    let n: u32 = 2;
+    increment_by(&mut obj, n);
+    println!("{}", obj.counter.to_string()); // Outputs: 7
+    println!("{}", n.to_string()); // Outputs: 2
 }
